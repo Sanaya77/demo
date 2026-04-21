38b66e6198305f7a25f8f03c36978c8a2b16b1b8
Sanaya Kulkarni <145009804+Sanaya77@users.noreply.github.com>
2026-04-22 00:10:30 +0530

    Refactor FIFO creation and file descriptor handling
    Updated FIFO handling by adding unlinking of existing FIFOs before creation and changed file descriptors to O_RDWR for both FIFOs.

--- fifo_p1.c
+++ fifo_p1.c
@@ -12,18 +12,33 @@ int main()
     int fd1, fd2;
     char input[SIZE], output[SIZE];
 
+   
+    unlink("fifo1");
+    unlink("fifo2");
+
+    
     mkfifo("fifo1", 0666);
     mkfifo("fifo2", 0666);
 
-    fd1 = open("fifo1", O_WRONLY);
-    fd2 = open("fifo2", O_RDONLY);
+  
+    fd1 = open("fifo1", O_RDWR);
+    fd2 = open("fifo2", O_RDWR);
 
-    printf("Enter sentences (Ctrl+D to stop):\n");
+    if (fd1 < 0 || fd2 < 0)
+    {
+        printf("Error opening FIFO\n");
+        return 1;
+    }
+
+    printf("Enter sentence: ");
     fgets(input, SIZE, stdin);
 
+    
     write(fd1, input, strlen(input) + 1);
 
+    
     read(fd2, output, SIZE);
+
     printf("\nResult from Process 2:\n%s\n", output);
 
     close(fd1);
@@ -31,4 +46,3 @@ int main()
 
     return 0;
 }
-
